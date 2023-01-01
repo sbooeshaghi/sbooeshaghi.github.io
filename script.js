f5e6c0aade8d803253316ebb1db11bd6806645bd
Sina Booeshaghi <sbooeshaghi@gmail.com>
2023-01-01 13:39:02 -0400

    clean up code

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function copyOnClick() {
-  var element = document.getElementById("creditName");
-  var text = element.innerText;
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(text);
-
-  // Alert the copied text
-  alert("Copied the text: " + text);
-}
+// get the text from the DOM Element:
+// when someone clicks on the <a class="copy-text"> element
+// (which should be a <button>), execute the copy command:
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".copy-text").addEventListener("click", () => {
+    const textToCopy = document.getElementById("creditName").innerText;
+    navigator.clipboard.writeText(textToCopy).then(
+      function () {
+        /* clipboard successfully set */
+        window.alert("Success! The text was copied to your clipboard");
+      },
+      function () {
+        /* clipboard write failed */
+        window.alert("Opps! Your browser does not support the Clipboard API");
+      }
+    );
+  });
+});
